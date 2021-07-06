95a68d6861079b1415568a350b189f8e7b716fb1
Zhuo Chen <zhuo.chen@compass.com>
2021-07-06 22:27:44 +0800

    adjust formatting

--- command/standup-rot.ts
+++ command/standup-rot.ts
@@ -5,7 +5,7 @@ export function doStandupRot(argMap: CommandArgumentMap) {
 
     const host = "abc",
         secretary = "xyz";
-    return `${at} **站会轮岗**
-:microphone: 主持: ${host}
-:writing_hand: 记录: ${secretary}`;
+    return `${at} *站会轮岗*
+:microphone: 主持 ${host}
+:writing_hand: 记录 ${secretary}`;
 }
